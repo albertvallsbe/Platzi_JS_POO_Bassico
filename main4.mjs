d842c6bfcf9f7cd09675956e843d0691a2e56339
Albert <avalls89@gmail.com>
2022-01-27 17:18:02 +0100

    27/01/22

--- main4.mjs
+++ main4.mjs
@@ -1,16 +1,74 @@
+//Partim de main3.js
+//Modificat a la classe 11/17 de Poo en JavaScript
+
+function videoPlay(id){
+    const urlSecreta = "https://platzisecret.com/" + id;
+    console.log("se esta reproduciendo desde la url " + urlSecreta);
+};
+
+
+function videoStop(id){
+    const urlSecreta = "https://platzisecret.com/" + id;
+    console.log("Pausamos la url " + urlSecreta);
+};
+
+export class PlatziClass {
+    constructor({
+        name,
+        videoID,
+    }){
+        this.name = name;
+        this.videoID = videoID;
+    }
+
+    reproducir(){
+        videoPlay(this.videoID);
+    }
+
+    pausar(){
+        videoStop(this.videoID);
+    }
+}
+
+
 class Course {
     constructor({
         name,
         classes = [],
     }) {
-        this.name = name;
+        this._name = name;
         this.classes = classes;
     }
-}
+    get name(){
+        return this._name;
+    }
+
+    changeName(nuevoNombre){
+        this._name= nuevoNombre
+    }
+
+    set name(nuevoNombre){
+        if(nuevoNombre === "Curso Malo"){
+            console.error("Web.... no");
+        }else {
+            this._name = nuevoNombre;
+        }
+    }
+};
+
+
+
 
 const cursoProgBasica = new Course({
-    name: "Curso Basico de Programación Básica",
+    name: "Curso gratis de Programación Básica",
 });
+
+//exemple
+cursoProgBasica.name;
+// Curso gratis de Programación Básica
+//per canviar el nom:
+//cursoProgBasica.changeName("Pepe Programa");
+
 const cursoDefinitivoHTML = new Course({
     name: "Curso Definitivo de HTML y CSS",
 });
---
+++ module.mjs
@@ -0,0 +1,16 @@
+import { PlatziClass } from "./main4.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript POO, basado en prototipos",
+    videoID: "1641638463",
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
